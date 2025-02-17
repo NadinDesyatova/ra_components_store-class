3009641dfce76c222e12ca0be2b1a3e716c2c2ff
NadinDesyatova <nadindinina@yandex.ru>
2025-02-17 22:12:57 +0300

    const item has been moved to App

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,16 @@ import Item from './components/shop-item-class/item';
 import ShopItemClass from './components/shop-item-class/ShopItemClass';
 
 
-const item = new Item(
-  'Tiger of Sweden',
-  'Leonard coat',
-  'Minimalistic coat in cotton-blend',
-  'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
-  399,
-  '£'
-);
-
 function App() {
+  const item = new Item(
+    'Tiger of Sweden',
+    'Leonard coat',
+    'Minimalistic coat in cotton-blend',
+    'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+    399,
+    '£'
+  );
+  
   return (
     <div className="container">
       <div className="background-element">
